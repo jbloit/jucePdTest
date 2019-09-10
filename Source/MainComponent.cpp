2d263c0cd13e34cb3143e034188580790f52769a
Julien Bloit <julien.bloit@gmail.com>
2019-09-10 12:25:41 +0200

    generate white noise

---
+++ Source/MainComponent.cpp
@@ -0,0 +1,96 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    // Make sure you set the size of the component after
+    // you add any child components.
+    setSize (800, 600);
+
+    // Some platforms require permissions to open input channels so request that here
+    if (RuntimePermissions::isRequired (RuntimePermissions::recordAudio)
+        && ! RuntimePermissions::isGranted (RuntimePermissions::recordAudio))
+    {
+        RuntimePermissions::request (RuntimePermissions::recordAudio,
+                                     [&] (bool granted) { if (granted)  setAudioChannels (2, 2); });
+    }
+    else
+    {
+        // Specify the number of input and output channels that we want to open
+//        setAudioChannels (2, 2);
+        setAudioChannels (0, 2); // no inputs, two outputs
+    }
+}
+
+MainComponent::~MainComponent()
+{
+    // This shuts down the audio device and clears the audio source.
+    shutdownAudio();
+}
+
+//==============================================================================
+void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+{
+    // This function will be called when the audio device is started, or when
+    // its settings (i.e. sample rate, block size, etc) are changed.
+
+    // You can use this function to initialise any resources you might need,
+    // but be careful - it will be called on the audio thread, not the GUI thread.
+
+    // For more details, see the help for AudioProcessor::prepareToPlay()
+
+    String message;
+    message << "Preparing to play audio..." << newLine;
+    message << " samplesPerBlockExpected = " << samplesPerBlockExpected << newLine;
+    message << " sampleRate = " << sampleRate;
+    Logger::getCurrentLogger()->writeToLog (message);
+
+    
+}
+
+void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill)
+{
+    // Your audio-processing code goes here!
+
+    for (auto channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel ){
+        auto* buffer = bufferToFill.buffer->getWritePointer(channel, bufferToFill.startSample);
+        
+        for (auto sample = 0; sample < bufferToFill.numSamples; ++sample){
+            buffer[sample] = random.nextFloat() * 0.25f - 0.125f;
+        }
+    }
+}
+
+void MainComponent::releaseResources()
+{
+    // This will be called when the audio device stops, or when it is being
+    // restarted due to a setting change.
+
+    // For more details, see the help for AudioProcessor::releaseResources()
+    
+     Logger::getCurrentLogger()->writeToLog ("Releasing audio resources");
+}
+
+//==============================================================================
+void MainComponent::paint (Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+    // You can add your drawing code here!
+}
+
+void MainComponent::resized()
+{
+    // This is called when the MainContentComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+}
